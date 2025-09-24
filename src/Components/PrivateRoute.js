f150e777fd545547c2a6f57b5365811d2862027c
Ananya <ananyabhardwaj0729@gmail.com>
2025-09-24 22:47:50 +0530

    i have updated the whatsapp no.

---
+++ src/Components/PrivateRoute.js
@@ -0,0 +1,10 @@
+// // PrivateRoute.js
+// import { Navigate } from "react-router-dom";
+
+// const PrivateRoute = ({ children }) => {
+//   const isAuthenticated = localStorage.getItem("token"); // or context/state
+
+//   return isAuthenticated ? children : <Navigate to="/login" replace />;
+// };
+
+// export default PrivateRoute;
